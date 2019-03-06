95b4bc2b6d9608635448ac9bc66636953325dc8b
unknown <benjamincligny@gmail.com>
2019-03-06 10:49:01 +0100

    classe transition

--- src/compilation/Transition.java
+++ src/compilation/Transition.java
@@ -1,4 +1,55 @@
 package compilation;
 
-public class Transition {
+import java.util.Objects;
+
+class Transition {
+
+    public static char meta = '#';
+    private final Etat etat;
+    private final char sortie;
+
+    Transition() {
+        this.etat = new Etat();
+        this.sortie = Transition.meta;
+    }
+
+    Transition(Etat e) {
+        this.etat = e;
+        this.sortie = Transition.meta;
+    }
+
+    Transition(Etat e, char s) {
+        this.etat = e;
+        this.sortie = s;
+    }
+
+    Etat getEtat() {
+        return this.etat;
+    }
+
+    Character getSortie() {
+        return this.sortie;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transition that = (Transition) o;
+        return sortie == that.sortie &&
+                Objects.equals(etat, that.etat);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(etat, sortie);
+    }
+
+    @Override
+    public String toString() {
+        return "Transition{" +
+                "etat=" + etat +
+                ", sortie=" + sortie +
+                '}';
+    }
 }
