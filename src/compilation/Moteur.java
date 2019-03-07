8f65a80a826d78d33e41e841fdb2e715fb764fe4
justinnorth <ZCkMx4M*CdWsk>
2019-03-07 16:43:04 +0100

    Moteur avec lecture du fichier descr terminé

--- src/compilation/Moteur.java
+++ src/compilation/Moteur.java
@@ -4,34 +4,43 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Représentation de l'automate
  */
 public class Moteur {
+    /**
+     * Dossier dans lequel se trouve les fichiers .descr
+     */
     private final String DOSSIER = "files/";
-    private static char meta = '#';
-    private String C, V, O;
-    private int E;
+
+
+    private String commentaire;
+    private Set<Character> vocabulaireEntree, vocabulaireSortie;
+    private int nbEtats;
 
     private ArrayList<Etat> lesEtats;
-    private ArrayList<Etat> I;
-    private ArrayList<Etat> F;
+    private ArrayList<Etat> etatsInitiaux;
+    private ArrayList<Etat> etatsAcceptants;
 
     /**
      * Constructeur par défaut
      */
     public Moteur() {
         this.lesEtats = new ArrayList<>();
-        this.lesEtats.add(new Etat("0", true, false));
-        this.C = "sansNom";
-        this.V = "";
-        this.O = "";
-        this.E = this.lesEtats.size();
-        this.I = new ArrayList<>();
-        this.I.add(this.lesEtats.get(0));
+        this.lesEtats.add(new Etat("0", true, false)); // Ajout d'un état nommé 0, initial
 
-        this.F = new ArrayList<>();
+        this.commentaire = "sansNom";
+        this.vocabulaireEntree = new TreeSet<>();
+        this.vocabulaireSortie = new TreeSet<>();
+        this.nbEtats = this.lesEtats.size();
+
+        this.etatsInitiaux = new ArrayList<>();
+        this.etatsInitiaux.add(this.lesEtats.get(0));
+
+        this.etatsAcceptants = new ArrayList<>();
     }
 
     /**
@@ -51,7 +60,7 @@ public class Moteur {
     /**
      * Lecture d'un fichier .descr
      * @param f fichier .descr
-     * @throws IOException
+     * @throws IOException Exception pour l'ouvertur du fichier
      */
     private void lireFichierDescr(String f) throws IOException {
         try {
@@ -66,37 +75,42 @@ public class Moteur {
                 ++numLigne;
                 switch (ligne.charAt(0)) {
                     case 'C':
-                        this.C = ligne.substring(3, ligne.length() - 1).trim();
+                        this.commentaire = ligne.substring(3, ligne.length() - 1).trim();
                         break;
                     case 'M':
                         char meta = ligne.charAt(3);
+                        if(!Transition.meta.equals(meta))
+                            Transition.meta = meta;
                         break;
                     case 'V':
-                        this.V = ligne.substring(3, ligne.length() - 2);
+                        String l = ligne.substring(3, ligne.length() - 2);
+                        for(int i = 0; i < l.length(); i++)
+                            this.vocabulaireEntree.add(l.charAt(i));
                         break;
                     case 'O':
-                        this.O = ligne.substring(3, ligne.length() - 2);
+                        l = ligne.substring(3, ligne.length() - 2);
+                        for(int i = 0; i < l.length(); i++)
+                            this.vocabulaireSortie.add(l.charAt(i));
                         break;
                     case 'E':
-                        this.E = Integer.parseInt(ligne.substring(2));
-                        for (int i = 1; i < this.E; ++i) {
+                        this.nbEtats = Integer.parseInt(ligne.substring(2));
+                        for (int i = 1; i < this.nbEtats; ++i)
                             this.lesEtats.add(new Etat(Integer.toString(i)));
-                        }
                         break;
                     case 'I':
                         this.lesEtats.get(0).setEstInitial(false);
-                        this.I.clear();
+                        this.etatsInitiaux.clear();
                         for (String nomEtat : ligne.substring(2).split(" ")) {
                             Etat etat = this.lesEtats.get(Integer.parseInt(nomEtat));
                             etat.setEstInitial(true);
-                            this.I.add(etat);
+                            this.etatsInitiaux.add(etat);
                         }
                         break;
                     case 'F':
                         for (String nomEtat : ligne.substring(2).split(" ")) {
                             Etat etat = this.lesEtats.get(Integer.parseInt(nomEtat));
                             etat.setEstAcceptant(true);
-                            this.F.add(etat);
+                            this.etatsAcceptants.add(etat);
                         }
                         break;
                     case 'T':
@@ -109,7 +123,7 @@ public class Moteur {
                         fin = this.lesEtats.get(Integer.parseInt(ligneSplit[2]));
 
                         entree = ligneSplit[1].charAt(1);
-                        sortie = Moteur.meta;
+                        sortie = Transition.meta;
 
                         if (ligneSplit.length > 3) {
                             if (sortie != ligneSplit[3].charAt(1))
